d5c368c9963a7dc19ed00a2f84b57531649b2e31
Kekeu <klebersongaldinoss@gmail.com>
2025-05-12 21:22:41 -0300

    Funcionalidades javascript

--- renderer.js
+++ renderer.js
@@ -1,3 +1,54 @@
+const button = document.querySelector('.add-task');
+const inputTask = document.querySelector('.input-task');
+const inputTime = document.querySelector('.input-time');
+const fullList = document.querySelector('.ul-list');
+
+let taskList = []
+
+function adicionarTask(){
+    taskList.push({
+      tarefa: inputTask.value,
+      hora: inputTime.value,
+      concluida: false
+    })
+  //taskList.push(inputTime.value)
+
+    inputTask.value = '';
+    inputTime.value ='';
+    mostrarTarefas();
+}
+
+function mostrarTarefas(){
+
+    let novaLi = ''
+
+    taskList.forEach((item, index) => {
+
+      novaLi = novaLi + `
+
+      <li class="tarefa">
+          <div class="dcheck">
+              <img class="img-close" src="assets/close_76dp_E8EAED_FILL0_wght400_GRAD0_opsz48.png" alt="Fechar" onclick="deletItem(${index})">
+              <img class="img-check" src="assets/check_box_outline_blank_76dp_E8EAED_FILL0_wght400_GRAD0_opsz48.png" alt="Caixa de Check">
+          </div>
+            <p class="ltask">${item.tarefa}</p>
+            <p class="ltime">09:00</p>
+      </li> 
+
+      `
+    })
+    fullList.innerHTML = novaLi;
+}
+
+function deletItem(index){
+    taskList.splice(index, 1)
+    
+    mostrarTarefas();
+}
+
+button.addEventListener('click', adicionarTask)
+
+/*
 // Textos das notificações
 const NOTIFICATION_TITLE = 'Title';
 const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.';
@@ -20,4 +71,4 @@ function showNotification() {
   const notification = new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY });
   
   
-}
+}*/
